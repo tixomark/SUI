f748fd2646a7b7a535080b845fdc04a93890b6ca
tixomark <tixomark@mail.ru>
2024-04-29 13:08:52 +0300

    initial commit

---
+++ SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by Tixon Markin on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
